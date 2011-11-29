93d9886af731c943dac2277132072f848952b05c
mmusial <mmusial@d0e49f55-ba13-a545-a6eb-e8ae84bb29ea>
2011-11-29 22:33:24 +0000

    + added parser and renderer classes * moved parser and renderer code to own respective classes * updated init.php to respect the above changes
    git-svn-id: svn://localhost/Personal/kohana-modules/branches/3.2/restful@1313 d0e49f55-ba13-a545-a6eb-e8ae84bb29ea

---
+++ classes/restful/requestparser.php
@@ -0,0 +1,51 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * RequestParser Interface
+ *
+ * @package		RESTful
+ * @author		Michał Musiał
+ * @copyright	(c) 2011 Michał Musiał
+ */
+class RESTful_RequestParser
+{
+	/**
+	 * @var	array
+	 */
+	protected static $_parsers = array();
+	
+	/**
+	 * @param	string $type
+	 * @return	mixed Returns all parsers if $type not specified, a parser callback if found or boolean FALSE otherwise
+	 */
+	public static function get($type = NULL)
+	{
+		if ($type === NULL)
+		{
+			return self::$_parsers;
+		}
+		else
+		{
+			return (array_key_exists($type, self::$_parsers)) ? self::$_parsers[$type] : FALSE;
+		}
+	}
+	
+	/**
+	 * @param	string $type Content MIME type
+	 * @param	callback $callback
+	 * @return	mixed Returns previous parser if one existed or TRUE otherwise
+	 */
+	public static function register($type, $callback)
+	{
+		if (array_key_exists($type, self::$_parsers))
+		{
+			$return = self::$_parsers[$type];
+		}
+		else
+		{
+			$return = TRUE;
+		}
+		
+		self::$_parsers[$type] = $callback;
+		return $return;
+	}
+}

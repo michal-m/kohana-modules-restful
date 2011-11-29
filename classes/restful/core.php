3280edb74c1454abf2e91e8b9df4b9bcb53a1242
mmusial <mmusial@d0e49f55-ba13-a545-a6eb-e8ae84bb29ea>
2011-11-29 12:13:19 +0000

    * added register_parser(), register_renderer(), parsers(), renderers() static methods
    git-svn-id: svn://localhost/Personal/kohana-modules/branches/3.2/restful@1289 d0e49f55-ba13-a545-a6eb-e8ae84bb29ea

--- classes/restful/core.php
+++ classes/restful/core.php
@@ -7,6 +7,90 @@
 class RESTful_Core
 {
 	/**
+	 * @var array
+	 */
+	protected static $_parsers = array();
+	
+	/**
+	 * @var array
+	 */
+	protected static $_renderers = array();
+	
+	
+	
+	/**
+	 * @param string $type Content MIME type
+	 * @param callback $callback
+	 * @return mixed Returns previous parser if one existed or TRUE otherwise
+	 */
+	public static function register_parser($type, $callback)
+	{
+		if (array_key_exists($type, self::$_parsers))
+		{
+			$return = self::$_parsers[$type];
+		}
+		else
+		{
+			$return = TRUE;
+		}
+		
+		self::$_parsers[$type] = $callback;
+		return $return;
+	}
+	
+	/**
+	 * @param string $type Content MIME type
+	 * @param callback $callback
+	 * @return mixed Returns previous renderer if one existed or TRUE otherwise
+	 */
+	public static function register_renderer($type, $callback)
+	{
+		if (array_key_exists($type, self::$_renderers))
+		{
+			$return = self::$_renderers[$type];
+		}
+		else
+		{
+			$return = TRUE;
+		}
+		
+		self::$_renderers[$type] = $callback;
+		return $return;
+	}
+	
+	/**
+	 * @param string $type
+	 * @return mixed Returns all parsers if $type not specified, a parser callback if found or boolean FALSE otherwise
+	 */
+	public static function parsers($type = NULL)
+	{
+		if ($type === NULL)
+		{
+			return self::$_parsers;
+		}
+		else
+		{
+			return (array_key_exists($type, self::$_parsers)) ? self::$_parsers[$type] : FALSE;
+		}
+	}
+
+	/**
+	 * @param string $type
+	 * @return mixed Returns all renderers if $type not specified, a parser callback if found or boolean FALSE otherwise
+	 */
+	public static function renderers($type = NULL)
+	{
+		if ($type === NULL)
+		{
+			return self::$_renderers;
+		}
+		else
+		{
+			return (array_key_exists($type, self::$_renderers)) ? self::$_renderers[$type] : FALSE;
+		}
+	}
+
+	/**
 	 * Inline exception handler, displays the error message, source of the
 	 * exception, and the stack trace of the error.
 	 *

c226b170ab854c4cb07aae8767ec61755ae1d407
MichalM <michal.j.musial@gmail.com>
2013-03-08 18:37:18 +0100

    consolidated response renderers into one class also json is now not formatted anymore

---
+++ classes/RESTful/Response/Renderer.php
@@ -0,0 +1,87 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * JSON Data Response Renderer class.
+ *
+ * @package     RESTful
+ * @category    Renderers
+ * @author      Michał Musiał
+ * @copyright   (c) 2013 Michał Musiał
+ */
+class RESTful_Response_Renderer {
+
+    /**
+     * Renderer for application/json mime type.
+     *
+     * @param   mixed $input
+     * @return  string
+     */
+    static public function application_json($data)
+    {
+        $json = json_encode($data);
+
+        if (json_last_error() !== JSON_ERROR_NONE)
+        {
+            throw HTTP_Exception::factory(500);
+        }
+
+        return $json;
+    }
+
+    /**
+     * Renderer for application/php-serialized mime type.
+     *
+     * @param   mixed $input
+     * @return  string
+     */
+    static public function application_php_serialized($data)
+    {
+        return serialize($data);
+    }
+
+    /**
+     * Renderer for application/php-serialized-array mime type.
+     *
+     * @param   mixed $input
+     * @return  string
+     */
+    static public function application_php_serialized_array($data)
+    {
+        return serialize((array) $data);
+    }
+
+    /**
+     * Renderer for application/php-serialized-object mime type.
+     *
+     * @param   mixed   $input
+     * @return  string
+     */
+    static public function application_php_serialized_object($data)
+    {
+        return serialize((object) $data);
+    }
+
+    /**
+     * Renderer for text/php-printr mime type.
+     *
+     * @param   mixed $input
+     * @return  string
+     */
+    static public function text_php_printr($data)
+    {
+        return print_r($data, TRUE);
+    }
+
+    /**
+     * Renderer for text/plain mime type.
+     *
+     * @param   mixed $input
+     * @return  string
+     */
+    static public function text_plain($data)
+    {
+        if (is_object($data) AND ! method_exists($data, '__toString'))
+            return 'Object of ' . get_class($data) . ' class';
+        else
+            return (string) $data;
+    }
+}
--- init.php
+++ init.php
@@ -5,9 +5,9 @@ RESTful_Request::parser('application/php-serialized',               'RESTful_Req
 RESTful_Request::parser('application/x-www-form-urlencoded',        'RESTful_Request_Parser::application_x_www_form_urlencoded');
 RESTful_Request::parser('text/plain',                               'RESTful_Request_Parser::text_plain');
 
-RESTful_Response::renderer('application/json',                     'RESTful_Response_Renderer_JSON::render');
-RESTful_Response::renderer('application/php-serialized',           'RESTful_Response_Renderer_PHP::render');
-RESTful_Response::renderer('application/php-serialized-array',     'RESTful_Response_Renderer_PHP_Array::render');
-RESTful_Response::renderer('application/php-serialized-object',    'RESTful_Response_Renderer_PHP_Object::render');
-RESTful_Response::renderer('text/php-printr',                      'RESTful_Response_Renderer_PRINTR::render');
-RESTful_Response::renderer('text/plain',                           'RESTful_Response_Renderer_PLAIN::render');
+RESTful_Response::renderer('application/json',                     'RESTful_Response_Renderer::application_json');
+RESTful_Response::renderer('application/php-serialized',           'RESTful_Response_Renderer::application_php_serialized');
+RESTful_Response::renderer('application/php-serialized-array',     'RESTful_Response_Renderer::application_php_serialized_array');
+RESTful_Response::renderer('application/php-serialized-object',    'RESTful_Response_Renderer::application_php_serialized_object');
+RESTful_Response::renderer('text/php-printr',                      'RESTful_Response_Renderer::text_php_printr');
+RESTful_Response::renderer('text/plain',                           'RESTful_Response_Renderer::text_plain');

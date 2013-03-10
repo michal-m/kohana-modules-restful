d4444bd3138bd055eb49dba50ff583259c0c790f
MichalM <michal.j.musial@gmail.com>
2013-03-11 00:39:11 +0100

    added execute() method that throws 405 instead of the default 404

--- classes/RESTful/Controller.php
+++ classes/RESTful/Controller.php
@@ -46,6 +46,46 @@ abstract class RESTful_Controller extends Controller {
      */
     protected $_request_data;
 
+	/**
+	 * Executes the given action and calls the [Controller::before] and [Controller::after] methods.
+	 *
+	 * Can also be used to catch exceptions from actions in a single place.
+	 *
+	 * 1. Before the controller action is called, the [Controller::before] method
+	 * will be called.
+	 * 2. Next the controller action will be called.
+	 * 3. After the controller action is called, the [Controller::after] method
+	 * will be called.
+	 *
+	 * @throws  HTTP_Exception_405
+	 * @return  Response
+	 */
+	public function execute()
+	{
+		// Execute the "before action" method
+		$this->before();
+
+		// Determine the action to use
+		$action = 'action_'.$this->request->action();
+
+		// If the action doesn't exist, it's a 405 Method Not Allowed
+		if ( ! method_exists($this, $action))
+		{
+            throw HTTP_Exception::factory(405)
+                ->headers('Allow', implode(', ', array_keys($this->_action_map)))
+                ->request($this->request);
+		}
+
+		// Execute the action itself
+		$this->{$action}();
+
+		// Execute the "after action" method
+		$this->after();
+
+		// Return the response
+		return $this->response;
+	}
+
     /**
      * Preflight checks.
      */
